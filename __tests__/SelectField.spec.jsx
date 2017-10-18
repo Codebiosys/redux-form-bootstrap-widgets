2381a7736fdfad56328dbaee1efba5664c8b371a
David Mote <davidmote@gmail.com>
2017-10-18 16:34:46 -0700

    100% coverage on Select field and utils.

---
+++ __tests__/SelectField.spec.jsx
@@ -0,0 +1,133 @@
+/* eslint-disable import/first */
+
+import React from 'react';
+import { mount } from 'enzyme';
+import Select from 'react-select';
+import { SelectField } from 'index';
+
+const fieldProps = {
+  input: {
+    name: 'fieldName',
+    onChange: jest.fn(),
+    onBlur: jest.fn(),
+    onFocus: jest.fn(),
+  },
+  meta: {
+    form: 'formName',
+    pristine: false,
+  },
+  label: 'Radio Test',
+  helpText: 'The help Text',
+  options: [
+    { label: 'Option 1', value: 'One' },
+    { label: 'Option 2', value: 'Two' },
+    { label: 'Option 3', value: 'Three' },
+  ],
+};
+
+const selectedFieldProps = { ...fieldProps };
+selectedFieldProps.input = { ...fieldProps.input };
+selectedFieldProps.input.value = 'One';
+
+const customOptionFieldProps = { ...fieldProps };
+customOptionFieldProps.labelKey = 'foo';
+customOptionFieldProps.valueKey = 'bar';
+customOptionFieldProps.options = [
+  { foo: 'Option 1', bar: 'One' },
+  { foo: 'Option 2', bar: 'Two' },
+  { foo: 'Option 3', bar: 'Three' }];
+
+
+const multipleFieldProps = { ...fieldProps };
+multipleFieldProps.multiple = true;
+
+const multiValueFieldProps = { ...multipleFieldProps };
+multiValueFieldProps.input = { ...multipleFieldProps.input };
+multiValueFieldProps.input.value = ['One', 'Two'];
+
+const asyncFieldProps = { ...fieldProps };
+delete asyncFieldProps.options;
+asyncFieldProps.loadOptions = input => Promise.resolve({
+  options: [
+          { label: 'one', value: 1 },
+          { label: 'two', value: 2 },
+          { label: 'three', value: 3 },
+  ],
+});
+
+describe('The Select Field', () => {
+  let inputWrapper;
+  let asyncWrapper;
+  let multipleWrapper;
+
+  beforeEach(() => {
+    inputWrapper = mount(<SelectField {...fieldProps} />);
+    asyncWrapper = mount(<SelectField {...asyncFieldProps} />);
+    multipleWrapper = mount(<SelectField {...multipleFieldProps} />);
+  });
+
+  it('renders', () => {
+    expect(inputWrapper).toMatchSnapshot();
+  });
+
+  it('renders with custom options', () => {
+    inputWrapper = mount(<SelectField {...customOptionFieldProps} />);
+    expect(inputWrapper).toMatchSnapshot();
+  });
+
+  it('renders with async options', () => {
+    expect(asyncWrapper).toMatchSnapshot();
+  });
+
+  it('renders allowing multiple selections', () => {
+    expect(multipleWrapper).toMatchSnapshot();
+  });
+  it('triggers the focus when focused', () => {
+    inputWrapper.find('.Select-input input').simulate('focus');
+    expect(fieldProps.input.onFocus).toHaveBeenCalledTimes(1);
+  });
+
+  it('triggers the async focus when async field is focused', () => {
+    asyncWrapper.find('.Select-input input').simulate('focus');
+    expect(asyncFieldProps.input.onFocus).toHaveBeenCalledTimes(1);
+  });
+
+  it('triggers the blur when blurred', () => {
+    inputWrapper.find('.Select-input input').simulate('blur');
+    expect(fieldProps.input.onBlur).toHaveBeenCalledTimes(1);
+  });
+
+  it('triggers the async blur when async field is blurred', () => {
+    asyncWrapper.find('.Select-input input').simulate('blur');
+    expect(asyncFieldProps.input.onBlur).toHaveBeenCalledTimes(1);
+  });
+
+  it('triggers the onChange when value is changed', () => {
+    inputWrapper.find(Select).prop('onChange')({ label: 'Option 2', value: 'Two' });
+    expect(fieldProps.input.onChange).toHaveBeenCalledTimes(1);
+    expect(fieldProps.input.onChange).toHaveBeenCalledWith('Two');
+  });
+
+  it('triggers the onChange with null when value is missing', () => {
+    inputWrapper.find(Select).prop('onChange')({ label: 'Option 4' });
+    expect(fieldProps.input.onChange).toHaveBeenCalledWith(null);
+  });
+
+  it('triggers the onChange when value is changed for multiple entry', () => {
+    multipleWrapper.find(Select).prop('onChange')({ label: 'Option 2', value: 'Two' });
+    expect(multipleFieldProps.input.onChange).toHaveBeenCalledTimes(1);
+    expect(multipleFieldProps.input.onChange).toHaveBeenCalledWith(['Two']);
+  });
+
+  it('adds to the value when  the onChange when value is changed for multiple entry', () => {
+    multipleWrapper = mount(<SelectField {...multiValueFieldProps} />);
+    multipleWrapper.find(Select).prop('onChange')([{ label: 'Option 1', value: 'One' }, { label: 'Option 3', value: 'Three' }]);
+    expect(multipleFieldProps.input.onChange).toHaveBeenCalledTimes(1);
+    expect(multipleFieldProps.input.onChange).toHaveBeenCalledWith(['One', 'Three']);
+  });
+
+  it('triggers the onChange with null when value is emtry', () => {
+    multipleWrapper.find(Select).prop('onChange')([]);
+    expect(multipleFieldProps.input.onChange).toHaveBeenCalledWith(null);
+  });
+});
---
+++ __tests__/utils.spec.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { HelpBlock } from 'react-bootstrap';
+
+import validationMessage from 'utils';
+
+describe('The validation message', () => {
+  it('returns undefined if the item is not touched', () => {
+    const { validationState, errorMessage } = validationMessage({});
+    expect(validationState).toEqual(undefined);
+    expect(errorMessage).toEqual(undefined);
+  });
+
+  it('returns success if the item is touched, and there are no errors or warnings', () => {
+    const { validationState, errorMessage } = validationMessage(
+      { touched: true,
+        error: undefined,
+        warning: undefined,
+      });
+    expect(validationState).toEqual('success');
+    expect(errorMessage).toEqual(undefined);
+  });
+
+  it('returns error with an error message if the item is touched, and there is an error', () => {
+    const { validationState, errorMessage } = validationMessage(
+      { touched: true,
+        error: 'An error Occured',
+        warning: undefined,
+      });
+    expect(validationState).toEqual('error');
+    expect(errorMessage).toEqual(<HelpBlock>An error Occured</HelpBlock>);
+  });
+
+  it('returns warning with an warning message if the item is touched, and there is a warning', () => {
+    const { validationState, errorMessage } = validationMessage(
+      { touched: true,
+        warning: 'An warning Occured',
+        error: undefined,
+      });
+    expect(validationState).toEqual('warning');
+    expect(errorMessage).toEqual(<HelpBlock>An warning Occured</HelpBlock>);
+  });
+
+  it('will prioritize errors over warnings', () => {
+    const { validationState, errorMessage } = validationMessage(
+      { touched: true,
+        warning: 'An warning Occured',
+        error: 'An error Occured',
+      });
+    expect(validationState).toEqual('error');
+    expect(errorMessage).toEqual(<HelpBlock>An error Occured</HelpBlock>);
+  });
+});
