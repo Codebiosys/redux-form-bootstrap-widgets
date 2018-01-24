a972808f14c320deb16f9d8be5e2168cb46ff9e2
David Mote <davidmote@gmail.com>
2018-01-24 09:43:47 -0800

    Add ToggleField Storybook

--- src/ToggleField.jsx
+++ src/ToggleField.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import {
   FormGroup,
   HelpBlock,
@@ -16,6 +18,7 @@ const ToggleField = ({
   label,
   required,
   helpText,
+  disabled,
   customValidation,
   input: { name, value, onChange, ...inputProps },
   meta,
@@ -39,6 +42,7 @@ const ToggleField = ({
           onChange={(event) => {
             onChange(!!event.target.checked);
           }}
+          disabled={disabled}
           {...props}
         />
       </InputGroup>
@@ -50,6 +54,37 @@ const ToggleField = ({
 };
 
 ToggleField.propTypes = {
-  ...Toggle.propTypes,
+  /** additional Props that can be passed to the Toggle Field */
+  ...ToggleField.propTypes,
+  /** Form label. */
+  label: PropTypes.string.isRequired,
+  /** Flag to display required Astrisk. */
+  required: PropTypes.bool,
+  /** Whether or not the field is disabled */
+  disabled: PropTypes.bool,
+  /** Additional text that displays below the widget. */
+  helpText: PropTypes.string,
+  /** HTML input type. */
+  customValidation: PropTypes.func,
+  /** React Boostrap Field addOn, placed before the input */
+  /**
+  * @ignore
+  * Redux Form internal input property. Set when used in a redux 'Field'
+  */
+  input: PropTypes.object.isRequired,
+
+  /**
+  * @ignore
+  * Redux Form internal meta property. Set when used in a redux 'Field'
+  */
+  meta: PropTypes.object.isRequired,
+
+};
+
+ToggleField.defaultProps = {
+  required: false,
+  disabled: false,
+  helpText: null,
+  customValidation: undefined,
 };
 export default ToggleField;
--- stories/ToggleField.jsx
+++ stories/ToggleField.jsx
@@ -2,17 +2,41 @@ import React from 'react';
 import { Field } from 'redux-form';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { withKnobs, text, boolean, number, select } from '@storybook/addon-knobs/react';
+import ReduxFormWrapper from '../.storybook/ReduxForm';
 
 import ToggleField from '../src/ToggleField';
 
+const storyConfig = {
+  propTables: [ToggleField],
+  propTablesExclude: [Field],
+};
+
 storiesOf('ToggleField', module)
-.addWithInfo('with text',
-'A Description',
- () => (
-   <Field
-     name="toggle_withtext"
-     component={ToggleField}
-     label="Toggle Field"
-     helpText="here to help"
-   />
-  ));
+  .addDecorator(withKnobs)
+  .addDecorator(ReduxFormWrapper('storytime'))
+  .addWithInfo('Default ToggleField Field',
+  `A ToggleField field with a react-bootstrap wrapping that connects to a redux field.
+   For fields and their functionality, see the
+   [redux-form documentation](https://redux-form.com/7.2.1/docs/api/field.md/).
+   Properties that are not required or optional for the Checkbox widget will be
+   passed on to the underlying
+   [react-toggle](http://aaronshaf.github.io/react-toggle/).
+  `,
+  () => (
+    <Field
+      name="fieldName"
+      component={ToggleField}
+      label={text('Label', 'Radio Field Label')}
+      required={boolean('Required', false)}
+      disabled={boolean('Disabled', false)}
+      helpText={text('Help Text', 'Help text for the widget')}
+      inline={boolean('Inline', false)}
+      options={[
+                { label: 'one', value: 1 },
+                { label: 'two', value: 2 },
+                { label: 'three', value: 3 },
+      ]}
+    />
+  ),
+  storyConfig);
